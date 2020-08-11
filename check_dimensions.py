0acbe7c9d2647a9e68ef952f85135e77cfd9ca6f
Jonathan Burkow <Jonathan.Burkow@gmail.com>
2020-08-11 00:10:46 -0400

    New file to check annotation offset dimensions

---
+++ check_dimensions.py
@@ -0,0 +1,63 @@
+'''
+Filename: check_dimensions.py
+Author: Jonathan Burkow, burkowjo@msu.edu
+        Michigan State University
+Last Updated: 08/10/2020
+Description: Loads in the dicom_offsets.csv file, and loops through all
+    cropped/equalized images to check whether the annotation offsets
+    fall outside the dimensions of the cropped images.
+'''
+
+import os
+import time
+import imageio
+import pandas as pd
+import args
+
+# Print out start of execution
+print('Starting execution...')
+start_time = time.perf_counter()
+
+# Create list of processed images
+img_folder_path = os.path.join(args.ARGS['8_BIT_FOLDER'],
+                               args.ARGS['CROPPED_EQUALIZED_IMAGE_FOLDER'])
+imgs = sorted(os.listdir(img_folder_path))
+
+# Load in offset annotation information
+anno_df = pd.read_csv(args.ARGS['ANNOTATION_OFFSET_FILENAME'],
+                      names=['PatientID', 'x1', 'y1', 'x2', 'y2', 'class'])
+
+# Drop class column
+anno_df = anno_df.drop('class', axis=1)
+
+# Remove paths from PatientID
+anno_df['PatientID'] = anno_df['PatientID'].str.replace(img_folder_path + '/', '')
+
+# Loop through images and store dimensions in a new DataFrame
+heights = []
+widths = []
+for i, img in enumerate(imgs):
+    img_path = os.path.join(img_folder_path, img)
+    image = imageio.imread(img_path)
+    heights.append(image.shape[0])
+    widths.append(image.shape[1])
+
+# Add PatientID, heights, and widths to a new DataFrame
+dim_df = pd.DataFrame({'PatientID' : imgs, 'Height' : heights, 'Width' : widths})
+
+# Merge the annotation DataFrame with the dimension DataFrame
+merged_df = anno_df.merge(dim_df, on='PatientID')
+
+# Make new colums based on whether annotation indices are outside dimensions
+merged_df['x1_outside'] = merged_df['x1'] > merged_df['Width']
+merged_df['x2_outside'] = merged_df['x2'] > merged_df['Width']
+merged_df['y1_outside'] = merged_df['y1'] > merged_df['Height']
+merged_df['y2_outside'] = merged_df['y2'] > merged_df['Height']
+
+# Save the DataFrame to a csv file
+merged_df.to_csv(args.ARGS['DIMENSION_CHECK_FILENAME'])
+
+# Print out time to complete
+print('Done!')
+end_time = time.perf_counter()
+print('Execution finished in {} seconds.'.format(round(end_time - start_time, 3)))
