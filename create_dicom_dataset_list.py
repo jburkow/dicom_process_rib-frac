cf61bed061a2a60ba17259233ffeb7bf39d3eb1b
Jonathan Burkow <jonathan.burkow@gmail.com>
2021-01-18 17:37:46 -0700

    Restructured

--- create_dicom_dataset_list.py
+++ create_dicom_dataset_list.py
@@ -2,7 +2,7 @@
 Filename: create_dicom_dataset_list.py
 Author: Jonathan Burkow, burkowjo@msu.edu
         Michigan State University
-Last Updated: 08/10/2020
+Last Updated: 01/18/2021
 Description: Goes through the provided dataset location of DICOM files
     and creates a file listing all which have annotations.
 
@@ -17,45 +17,52 @@ import time
 from pydicom import dcmread
 import args
 
-# Print out start of execution
-print('Starting execution...')
-start_time = time.perf_counter()
+def main():
+    """Main Function"""
+    # Load in InstanceUIDs
+    instance_uids = []
+    with open(args.ARGS['INSTANCE_UID_FILENAME'], 'r') as data_file:
+        csv_reader = csv.reader(data_file)
+        for line in csv_reader:
+            instance_uids.append(line[0])
 
-# Load in InstanceUIDs
-instance_uids = []
-with open(args.ARGS['INSTANCE_UID_FILENAME'], 'r') as data_file:
-    csv_reader = csv.reader(data_file)
-    for line in csv_reader:
-        instance_uids.append(line[0])
+    # Create a list of all DICOM files
+    full_dataset_list = []
+    for dirs, root, files in os.walk(args.ARGS['DICOM_FOLDER']):
+        for file in files:
+            full_dataset_list.append(os.path.join(dirs, file))
 
-# Create a list of all DICOM files
-full_dataset_list = []
-for dirs, root, files in os.walk(args.ARGS['DICOM_FOLDER']):
-    for file in files:
-        full_dataset_list.append(os.path.join(dirs, file))
+    # Create list of annotated DICOM files
+    annotated_dicoms = []
+    for dcm_file in os.listdir(args.ARGS['DICOM_FOLDER']):
+        file_path = os.path.join(args.ARGS['DICOM_FOLDER'], dcm_file)
+        dcm = dcmread(file_path)
 
-# Create list of annotated DICOM files
-annotated_dicoms = []
-for dcm_file in os.listdir(args.ARGS['DICOM_FOLDER']):
-    file_path = os.path.join(args.ARGS['DICOM_FOLDER'], dcm_file)
-    dcm = dcmread(file_path)
+        dcm_instance_uid = dcm.SOPInstanceUID
 
-    dcm_instance_uid = dcm.SOPInstanceUID
+        if dcm_instance_uid in instance_uids:
+            annotated_dicoms.append(file_path)
 
-    if dcm_instance_uid in instance_uids:
-        annotated_dicoms.append(file_path)
+    # Print out number of DICOM files and how many have pixel information
+    print('Total number of DICOM files:', len(full_dataset_list))
+    print('Annotated DICOM files:', len(annotated_dicoms))
 
-# Print out number of DICOM files and how many have pixel information
-print('Total number of DICOM files:', len(full_dataset_list))
-print('Annotated DICOM files:', len(annotated_dicoms))
+    # Save the list of paths to DICOM files with pixel information to a file
+    with open(args.ARGS['DATASET_LIST'], 'w+') as out_file:
+        for row in annotated_dicoms:
+            out_str = row + '\n'
+            out_file.write(out_str)
 
-# Save the list of paths to DICOM files with pixel information to a file
-with open(args.ARGS['DATASET_LIST'], 'w+') as out_file:
-    for row in annotated_dicoms:
-        out_str = row + '\n'
-        out_file.write(out_str)
 
-# Print out time to complete
-print('Done!')
-end_time = time.perf_counter()
-print('Execution finished in {} seconds.'.format(round(end_time - start_time, 3)))
+if __name__ == "__main__":
+    # Print out start of execution
+    print('Starting execution...')
+    start_time = time.perf_counter()
+
+    # Run main function
+    main()
+
+    # Print out time to complete
+    print('Done!')
+    end_time = time.perf_counter()
+    print('Execution finished in {} seconds.'.format(round(end_time - start_time, 3)))
